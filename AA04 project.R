abba684eb34cb38502beb0044af7625e8b77c6b1
preciousjane <pjstow@uw.edu>
2019-11-18 11:37:26 -0800

    data changes

--- AA04 project.R
+++ AA04 project.R
@@ -2,18 +2,20 @@
 
 # data frame work
 
-setwd("~/Desktop/info_project")
+setwd("~/Desktop/AA04")
 library(dplyr)
 library(leaflet)
 library(ggplot2)
+library(tidyr)
 
-#reading in CSV data
+#reading in CSV data -------------
+
 df <- read.csv("Voting_data.csv",
                header = TRUE,
                stringsAsFactors = FALSE
 )
 
-# deleting NA and unneeded columns
+# deleting NA and unneeded columns -------------
 
 new_df <- df %>%
   select(-X.15,-X.16,-X.17,-X.18)
@@ -23,12 +25,29 @@ vot_pop <- new_df %>%
   select(-X.3, -X.4, -X.5, -X.6, -X.7, -X.8, -X.9, -X.10, -X.11, -X.13, -X.14, -X.12)
 View(vot_pop)
 
-# changed column names - easier readability
+# changed column names - easier readability --------------
+
 data_frame <- setNames(vot_pop, c("Year","Total votes (all ages)  \n - by thousands",
                                   "total population (by percent)",
                                   "citizen population"))
 View(data_frame)
 
 # deleting uneeded rows
-removed_rows <- data_frame[-c(1:5, 35:37, 67:69, 100:102, 133:135, 166:168, 199:201),]
-  View(removed_rows)
+best_frame <- data_frame[-c(1:5, 35:37, 67:69, 100:102, 133:135, 166:168, 199:201),]
+  View(best_frame)
+  
+# pulling data to create new sub data frames for visulizations ------------
+  
+# All population who voted -----------
+  
+all_voted <- best_frame %>%
+  select(everything()) %>%
+  slice(2:29)
+View(all_voted)
+
+# All population who registered ---------
+all_registered <- best_frame %>%
+  select(everything()) %>%
+  slice(31:58)
+View(all_registered)
+  
