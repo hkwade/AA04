90faadec92c09dca5f06d2095393efadf15f9a11
raveenarao <iraveenarao@gmial.com>
2019-11-20 19:45:43 -0800

    final

--- AA04/app.R
+++ AA04/app.R
@@ -1,1 +1,22 @@
+# Data Visualizations
 
+library(ggplot2)
+
+df_three <- read.csv("Indivisual_state_data.csv",
+                     header = TRUE,
+                     stringsAsFactors = FALSE
+)
+   
+new_df_three <- df_three %>%
+  filter(State == "United States") %>%
+  select(X, State, Turnout.Rates) %>%
+  setNames(c("Year", "Location", "TurnoutRates")) %>% 
+  slice(1:9)
+View(new_df_three)
+
+line_graph <- ggplot(data = new_df_three, 
+                     aes(x = Year, y = TurnoutRates, group = 1)) +
+  geom_line(color = "BLACK") +
+  geom_point() +
+  ggtitle("Average Turnout Rates Per Year in America") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1))
